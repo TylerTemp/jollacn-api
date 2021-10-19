11681c664811c279671de88ba23ec579b278d00e
tyler <tyler@example.com>
2021-10-20 00:21:50 +0800

    pump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JollaCNAPI.MixProject do
   def project do
     [
       app: :jollacn_api,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
